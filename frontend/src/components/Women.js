2df48a81c6bafdff4372127087b2f3e6214c288b
Rezan92 <rezanibrahim8@gmail.com>
2021-10-19 10:46:14 +0200

    Created redux store

---
+++ frontend/src/components/Women.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Women = () => {
+  return (
+    <>
+      <h2 className='section-header'>Women Shoes</h2>
+    </>
+  );
+};
+
+export default Women;
--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,10 +1,12 @@
 import Carousel from '../components/Carousel';
+import Women from '../components/Women';
 
 const Home = () => {
   return (
     <>
       <Carousel />
       <div className='belt'></div>
+      <Women />
     </>
   );
 };
