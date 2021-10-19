482eeaa945397da4632213be82e9ea11226ff39e
Rezan92 <rezanibrahim8@gmail.com>
2021-10-19 11:55:44 +0200

    Added proxy to package.js

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 import Carousel from '../components/Carousel';
 import Women from '../components/Women';
 
 const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
   return (
     <>
       <Carousel />
