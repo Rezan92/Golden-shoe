6c5fb8c65bd674a60bfde795b01f55d4c7a4cca6
Rezan92 <rezanibrahim8@gmail.com>
2021-10-19 11:57:19 +0200

    Added cors package

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import allProduct from './products/router.js';
+import cors from 'cors';
+
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.use('/api/', allProduct);
 
