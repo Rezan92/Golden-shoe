c1e2e6b0f98c60ab6a5bdd1c160230e75b244512
Rezan92 <rezanibrahim8@gmail.com>
2021-10-19 12:22:23 +0200

    Get the products array in Home component

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,18 +1,29 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { listProducts } from '../actions/productActions';
 import Carousel from '../components/Carousel';
 import Women from '../components/Women';
 
 const Home = () => {
   const dispatch = useDispatch();
+  const store = useSelector((state) => state);
+  const { loading, error, products } = store.products;
+
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
+
   return (
     <>
       <Carousel />
       <div className='belt'></div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : error ? (
+        <h2>error</h2>
+      ) : (
+        products.map((product) => <h1 key={product.id}>{product.name}</h1>)
+      )}
       <Women />
     </>
   );
