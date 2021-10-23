6d39a9b0fd27be432d2a4c0cf663e0aa23701d4c
Rezan92 <rezanibrahim8@gmail.com>
2021-10-23 03:30:08 +0200

    Add cart item number

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,11 +7,13 @@ import {
   Form,
   Button,
 } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toggleCart } from '../actions/cartAction';
 
 const Header = () => {
   const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <>
       <Navbar bg='primary' expand='md' variant='dark' className='p-4'>
@@ -46,7 +48,12 @@ const Header = () => {
             <Nav.Link href='#action1' className='mx-1'>
               <FaHeart size='30' color='white' />
             </Nav.Link>
-            <Nav.Link href='#action1' className='mx-1'>
+            <Nav.Link
+              href='#action1'
+              className='mx-1'
+              className='item-num_container'
+            >
+              <div className='item-num'>{cartItems.length}</div>
               <FaShoppingBag
                 size='30'
                 color='white'
